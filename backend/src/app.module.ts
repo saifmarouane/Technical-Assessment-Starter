001e35c8c3cbdfe596ca42e39463092670b77abf
saifmarouane <96530039+saifmarouane@users.noreply.github.com>
2023-12-17 16:10:01 +0100

    update the .env file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
 import { AppResolver } from './app.resolver';
 import { GptAiApiModule } from './gpt_ai_api/gpt_ai_api.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SharedModule, GptAiApiModule],
+  imports: [SharedModule, GptAiApiModule,ConfigModule.forRoot()],
   providers: [AppResolver],
 })
 export class AppModule {}
