daf61c13913b7b5d9782d10a7996648d4f7b0e4b
saifmarouane <96530039+saifmarouane@users.noreply.github.com>
2023-12-22 23:16:01 +0100

    update the gitlab-ci.yml image:value=(curlimages/curl:latest)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ChatModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.graphql'),
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
